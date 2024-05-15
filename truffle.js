3f08f984528d870f6126512a08b20f770cf24eee
superdev313 <pablo313boss@gmail.com>
2024-05-15 15:41:37 -0500

    Contract for Nft Marketplace

---
+++ truffle.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*",
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_PID),
+      network_id: 4,
+      confirmations: 1,
+      timeoutBlocks: 10,
+      skipDryRun: true,
+      production: false,
+      gasPrice: 2000000000 // 2 gwei
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_PID),
+      network_id: 1,
+      confirmations: 3,
+      timeoutBlocks: 30,
+      skipDryRun: false,
+      production: true,
+      gas: 3750000,
+      gasPrice: 50000000000 // 50 gwei
+    },
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 2,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    'etherscan': process.env.ETHERSCAN_API
+  }
+};
