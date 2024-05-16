6e2a4301cf9bf58b1119aa6b4e8c0364def4483e
superdev313 <pablo313boss@gmail.com>
2024-05-16 08:20:15 -0500

    UpdateCollection

--- test/Marketplace721.test.js
+++ test/Marketplace721.test.js
@@ -11,8 +11,28 @@ contract("Marketplace ERC-721", function (accounts) {
   }
 
   beforeEach(async function () {
+    // deploy a new instance of Marketplace contract
     this.mp = await Marketplace.new({ from: accounts[0] });
     this.sample721 = await SampleProject721.new({ from: accounts[0] });
     await this.sample721.mint(10, { from: accounts[0] });
+  });
+
+  it("updateCollection requires contract ownership", async function () {
+    await expectRevert(
+      this.mp.updateCollection(this.sample721.address, false, 1, "", {
+        from: accounts[1],
+      }),
+      "You must own the contract."
+    );
+    await expectEvent(
+      await this.mp.updateCollection(
+        this.sample721.address,
+        false,
+        5,
+        "ipfs://ok",
+        { from: accounts[0] }
+      ),
+      "CollectionUpdated"
+    );
   });
 });
