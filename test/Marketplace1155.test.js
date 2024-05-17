aceb247e808000b7209a3713602322eeed773606
superdev313 <pablo313boss@gmail.com>
2024-05-16 21:43:41 -0500

    UpdateCollection

--- test/Marketplace1155.test.js
+++ test/Marketplace1155.test.js
@@ -19,4 +19,65 @@ contract('Marketplace ERC-1155', function(accounts) {
         await this.sample1155.mint(3, 1, {from: accounts[0]});
       });
     
+      
+  // updateCollection
+
+  it('updateCollection requires contract ownership', async function () {
+    await expectRevert(
+      this.mp.updateCollection(this.sample1155.address, true, 1, "", {from: accounts[1]}),
+      'You must own the contract.'
+    );
+    await expectEvent(
+      await this.mp.updateCollection(this.sample1155.address, true, 5, "ipfs://ok", {from: accounts[0]}),
+      'CollectionUpdated'
+    );
+  });
+
+  it('updateCollection updates each time', async function () {
+    // add/update collection as owner
+    await this.mp.updateCollection(this.sample1155.address, true, 1, "ipfs://mynewhash", {from: accounts[0]});
+    // settings should match
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).status
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).erc1155
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).royaltyPercent
+    ).to.be.bignumber.equal('1');
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).metadataURL
+    ).to.equal("ipfs://mynewhash");
+    // Disable collection (zero it out)
+    await this.mp.disableCollection(this.sample1155.address, {from: accounts[0]});
+    // update collection again
+    await this.mp.updateCollection(this.sample1155.address, true, 5, "ipfs://anothernewhash", {from: accounts[0]});
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).status
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).erc1155
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).royaltyPercent
+    ).to.be.bignumber.equal('5');
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).metadataURL
+    ).to.equal("ipfs://anothernewhash");
+    // update again
+    await this.mp.updateCollection(this.sample1155.address, true, 8, "ipfs://round3", {from: accounts[0]});
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).status
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).erc1155
+    ).to.equal(true);
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).royaltyPercent
+    ).to.be.bignumber.equal('8');
+    await expect(
+      (await this.mp.collectionState(this.sample1155.address)).metadataURL
+    ).to.equal("ipfs://round3");
+  });
 }
