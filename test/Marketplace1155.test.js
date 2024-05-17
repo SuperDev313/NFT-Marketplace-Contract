a2ee59f3d0a1a1642e83258215a334ce49db6e7c
superdev313 <pablo313boss@gmail.com>
2024-05-17 10:26:21 -0500

    AcceptOfferForToken gives contract owner their royalty in Martketplace1155 test script

--- test/Marketplace1155.test.js
+++ test/Marketplace1155.test.js
@@ -662,6 +662,42 @@ contract("Marketplace ERC-1155", function (accounts) {
     await expect(offerDetail.minValue).to.be.bignumber.equal(getPrice(0));
     await expect(offerDetail.onlySellTo).to.equal(nullAddress);
   });
+
+  it("acceptOfferForToken gives contract owner their royalty", async function () {
+    await this.mp.updateCollection(
+      this.sample1155.address,
+      true,
+      10,
+      "ipfs://mynewhash",
+      { from: accounts[0] }
+    );
+    await this.sample1155.mint(5, 10, { from: accounts[1] }); // mint 10 more as new address for token 5
+    await expect(
+      await this.sample1155.balanceOf(accounts[1], 5)
+    ).to.be.bignumber.equal("10");
+    await this.sample1155.setApprovalForAll(this.mp.address, true, {
+      from: accounts[1],
+    });
+    await this.mp.offerTokenForSale(this.sample1155.address, 5, getPrice(1), {
+      from: accounts[1],
+    });
+    await this.mp.acceptOfferForToken(this.sample1155.address, 5, {
+      from: accounts[2],
+      value: getPrice(1),
+    });
+    await expect(
+      await this.sample1155.balanceOf(accounts[1], 5)
+    ).to.be.bignumber.equal("9");
+    await expect(
+      await this.sample1155.balanceOf(accounts[2], 5)
+    ).to.be.bignumber.equal("1");
+    let ownerBalance = await this.mp.pendingBalance(accounts[0]);
+    // confirm 10% royalty for collection owner reflects in balances
+    // amount / (100 / royalty)
+    let royaltyAmount = 1 / (100 / 10);
+    await expect(ownerBalance).to.be.bignumber.equal(getPrice(royaltyAmount));
+  });
+
   it("acceptOfferForToken transfers the token to buyer", async function () {
     await this.mp.updateCollection(
       this.sample1155.address,
